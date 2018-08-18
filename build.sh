190ba0629916bebf4fb98da3ac587e3b1d2a9997
Pika <15848969+ThatNerdyPikachu@users.noreply.github.com>
2018-08-18 19:29:24 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ mkdir atmosphere
 mkdir atmosphere/titles
 mkdir atmosphere/titles/0100000000000036
 mkdir atmosphere/titles/0100000000000036/exefs
+mkdir atmosphere/titles/010000000000100D
+mkdir atmosphere/titles/010000000000100D/exefs
 mkdir atmosphere/exefs_patches
 mkdir PikaNX
 mkdir switch
@@ -24,6 +26,10 @@ cp /var/lib/jenkins/workspace/atmosphere-patched/stratosphere/pm/pm.kip PikaNX/p
 cp /var/lib/jenkins/workspace/atmosphere-patched/stratosphere/sm/sm.kip PikaNX/sm.kip
 cp /var/lib/jenkins/workspace/sys-ftpd/sys-ftpd.kip PikaNX/sys-ftpd.kip
 cp /var/lib/jenkins/workspace/sys-ftpd/sd_card/ftpd/* ftpd/
+cp /var/lib/jenkins/workspace/nx-hbmenu/nx-hbmenu.nro hbmenu.nro
+cp /var/lib/jenkins/nx-hbloader/hbl.nso atmosphere/titles/010000000000100D/exefs/main
+cp /var/lib/jenkins/nx-hbloader/hbl.npdm atmosphere/titles/010000000000100D/exefs/main.npdm
+touch atmosphere/titles/010000000000100D/exefs/rtld.stub
 cp /var/lib/jenkins/workspace/tinfoil/tinfoil.nro switch/tinfoil.nro
 cp -r "../Fake Tickets" atmosphere/exefs_patches
 cp /opt/BUILTBY.txt .
